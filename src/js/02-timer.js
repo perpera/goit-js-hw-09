5456c6ee193f67a00f0c1fe8cb0eedf727202001
perpera <131678085+perpera@users.noreply.github.com>
2023-10-02 15:33:23 +0300

    timer edit

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,6 +2,21 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
+const startBtn = document.querySelector('[data-start]');
+const resetBtn = document.querySelector('[data-reset]');
+const inputField = document.querySelector('#datetime-picker');
+let timerIsActive = false;
+let intervalId;
+
+const timerFields = {
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+resetBtn.disabled = true;
+
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -14,10 +29,10 @@ const options = {
 
     if (selectedDate <= currentDate) {
       Notiflix.Notify.failure('Please choose a date in the future');
-      document.querySelector('[data-start]').disabled = true;
+      startBtn.disabled = true;
     } else {
-      document.querySelector('[data-start]').disabled = false;
       Notiflix.Notify.success('Date selected successfully');
+      startBtn.disabled = false;
     }
   },
 };
@@ -25,20 +40,17 @@ const options = {
 flatpickr('#datetime-picker', options);
 
 function updateTimer(targetDate) {
-  const timerFields = {
-    days: document.querySelector('[data-days]'),
-    hours: document.querySelector('[data-hours]'),
-    minutes: document.querySelector('[data-minutes]'),
-    seconds: document.querySelector('[data-seconds]'),
-  };
-
   function updateDisplay() {
     const currentDate = new Date();
     const timeDif = targetDate - currentDate;
 
     if (timeDif <= 0) {
       clearInterval(intervalId);
+      timerIsActive = false;
       Object.values(timerFields).forEach(field => (field.textContent = '00'));
+      inputField.disabled = false;
+      startBtn.disabled = true;
+      resetBtn.disabled = true;
       return;
     }
 
@@ -52,34 +64,44 @@ function updateTimer(targetDate) {
     timerFields.minutes.textContent = format(minutes);
     timerFields.seconds.textContent = format(seconds);
   }
-
-  const intervalId = setInterval(updateDisplay, 1000);
+  intervalId = setInterval(updateDisplay, 1000);
   updateDisplay();
 }
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
 
-document.querySelector('[data-start]').addEventListener('click', () => {
-  const selectedDate = flatpickr.parseDate(
-    document.querySelector('#datetime-picker').value,
-    'Y-m-d H:i'
-  );
-  updateTimer(selectedDate);
+startBtn.addEventListener('click', () => {
+  if (!timerIsActive) {
+    const selectedDate = flatpickr.parseDate(inputField.value, 'Y-m-d H:i');
+    if (selectedDate <= new Date()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+    } else {
+      updateTimer(selectedDate);
+      timerIsActive = true;
+      inputField.disabled = true;
+      resetBtn.disabled = false;
+    }
+  }
+});
+
+resetBtn.addEventListener('click', () => {
+  clearInterval(intervalId);
+  timerIsActive = false;
+  Object.values(timerFields).forEach(field => (field.textContent = '00'));
+  inputField.disabled = false;
+  inputField.value = '';
+  resetBtn.disabled = true;
+  startBtn.disabled = true;
 });
